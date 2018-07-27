b77d9fbab963669954f9cfe4adcf9e76b83d7040
jixiang1 <jixiang1@corp.netease.com>
2018-07-27 17:25:55 +0800

    测试合并冲突的发生

--- src/main/java/jixiang/StringUtilss.java
+++ src/main/java/jixiang/StringUtilss.java
@@ -1,17 +1,10 @@
 package jixiang;
 
-import org.apache.commons.lang3.StringUtils;
+
 
 public class StringUtilss {
     public static void main(String[] args) {
-        System.out.println(StringUtils.isEmpty(null));
-        System.out.println(StringUtils.isBlank(null));
-        System.out.println(StringUtils.isEmpty(""));
-        System.out.println(StringUtils.isBlank(""));
-        System.out.println(StringUtils.isEmpty(" "));
-        System.out.println(StringUtils.isBlank(" "));
-        System.out.println(StringUtils.isEmpty("1"));
-        System.out.println(StringUtils.isBlank("1"));
+
 
     }
 
--- src/main/java/jixiang/Test.java
+++ src/main/java/jixiang/Test.java
@@ -1,6 +1,10 @@
 package jixiang;
 
 import java.sql.SQLOutput;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.LinkedList;
+import java.util.List;
 
 public class Test {
   public static void run(){
@@ -22,9 +26,38 @@ public class Test {
   }
 
   public static void main(String[] args){
-        Test.run();
+//        Test.run();
+//        String[] lists={"mayun","lilong","wangjun"};
+//      System.out.println(lists.length);
+//      System.out.println(lists.length);
+//      System.out.println(lists[0].length());
+//      System.out.println(lists[1].length());
+//      List<Integer> list=new LinkedList<Integer>();
+//      list.add(2);
+//      list.add(3);
+//      list.add(4);
+//      list.add(4);
+//      System.out.println(list.size());
+//      Boolean flag=null;
+//      System.out.println(Boolean.FALSE.equals(flag));
+//      App app=new App();
+//      System.out.println(App.class.getResource("/"));
+
+      Calendar calendar=Calendar.getInstance();
+      calendar.setTime(new Date());
+      System.out.println("现在的时间是："+new Date());
+      String year =String.valueOf(calendar.get(Calendar.YEAR));
+      String month=String.valueOf(calendar.get(Calendar.MONTH)+1);
+      String day=String.valueOf(calendar.get(Calendar.DAY_OF_MONTH));
+      String week=String.valueOf(calendar.get(Calendar.DAY_OF_WEEK)-1);
+      System.out.println("现在时间是："+year+"年"+month+"月"+day+"日，星期"+week);
+
+
+
   }
   public static void lll(){
+
+
       System.out.println("SSSSSS");
       System.out.println("MMMMMM");
       System.out.println("LLLLLL");
