7415e77acf746377ffd16e94ebeca4bbba33143f
jixiang1 <jixiang1@corp.netease.com>
2018-07-27 17:40:46 +0800

    测试合并冲突的发生

--- src/main/java/jixiang/TestController.java
+++ src/main/java/jixiang/TestController.java
@@ -3,6 +3,9 @@ package jixiang;
 
 public class TestController {
 
-    int i;
+    public static void main(String[] args) {
+        LogUse logUse=new LogUse();
+        logUse.doSomthing1();
+    }
 
 }
