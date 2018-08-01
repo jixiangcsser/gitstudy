797ce7b29990d61d072c3dffe862e044fc6a53f4
jixiang1 <jixiang1@corp.netease.com>
2018-08-01 09:54:43 +0800

    主线提交2

--- src/test/java/jixiang/Test.java
+++ src/test/java/jixiang/Test.java
@@ -7,4 +7,8 @@ public class Test {
     public void mainTest(){
         System.out.println("在main主线下的提交");
     }
+    @org.junit.Test
+    public void mainTest1(){
+        System.out.println("在main主线下的提交2");
+    }
 }
