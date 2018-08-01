367604ef2bbef1d8488f52e71c02f7ab3eb60a13
jixiang1 <jixiang1@corp.netease.com>
2018-08-01 15:07:51 +0800

    解决冲突

--- src/test/java/jixiang/TestTest.java
+++ src/test/java/jixiang/TestTest.java
@@ -35,13 +35,11 @@ public class TestTest {
         System.out.println("Jixiang分支上的修改2 Jixiang13:55  2223333");
     }
     public void mergeMaster4(){
-
-        System.out.println("这是姬祥的第5次修改!!");
+        System.out.println("这是master的第三次修改");
 
     }
     public void mergeMaster5(){
-        System.out.println("这是姬祥的第6次修改,就是要修改!!");
-
+        System.out.println("这是master的第四次修改");
     }
 
     public void jixiang1(){
