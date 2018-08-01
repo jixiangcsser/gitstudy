0a51e54c61dbf8b1f9e6c56bac7fdd1424f76a33
jixiang1 <jixiang1@corp.netease.com>
2018-08-01 15:37:10 +0800

    解决冲突

--- src/test/java/jixiang/TestTest.java
+++ src/test/java/jixiang/TestTest.java
@@ -40,7 +40,6 @@ public class TestTest {
     public void mergeMaster5(){
         System.out.println("这是master的第四次修改");
     }
-
     public void jixiang1(){
         System.out.println("姬祥第二次提交内容");
     }
