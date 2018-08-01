7d21eacea93ed2505bbed344f2505c6767ae51d1
jixiang1 <jixiang1@corp.netease.com>
2018-08-01 09:29:58 +0800

    测试rebase

--- src/main/java/jixiang/TestController.java
+++ src/main/java/jixiang/TestController.java
@@ -5,6 +5,12 @@ public class TestController {
 
     public static void main(String[] args) {
         LogUse logUse=new LogUse();
+        try {
+            Thread.sleep(100);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        NullPointerException nullPointerException=new NullPointerException();
         System.out.println("哦不行");
          logUse.doSomthing1();
         System.out.println("我想");
