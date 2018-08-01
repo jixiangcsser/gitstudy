45b3dcdf105f509ef4918ce600b78fdace8acacc
jixiang1 <jixiang1@corp.netease.com>
2018-08-01 10:15:36 +0800

    jixiang提交1

--- src/test/java/jixiang/Test.java
+++ src/test/java/jixiang/Test.java
@@ -15,4 +15,12 @@ public class Test {
     public void mainTest2(){
         System.out.println("在main主线下的提交3");
     }
+    @org.junit.Test
+    public void run4(){
+        System.out.println("这是jixiang分支上的第二次提交");
+    }
+    @org.junit.Test
+    public void run5(){
+        System.out.println("这是jixiang分支上的第三次提交");
+    }
 }
