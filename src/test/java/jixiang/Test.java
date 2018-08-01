4cabdfe1cb7fd813687b0b60140d2f62604be603
jixiang1 <jixiang1@corp.netease.com>
2018-08-01 10:26:26 +0800

    主线提交5

--- src/test/java/jixiang/Test.java
+++ src/test/java/jixiang/Test.java
@@ -14,7 +14,7 @@ public class Test {
     }
     @org.junit.Test
     public void mainTest(){
-        System.out.println("在main主线下的提交1,特已修改jixiang分支下修改1");
+        System.out.println("在main主线下的提交1,特已修改jixiang分支下修改");
     }
     @org.junit.Test
     public void mainTest4(){
